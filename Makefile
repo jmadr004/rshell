8fed5515094298ea92b3ff268c37fa1b8ebd5200
Joshua Madrid <jmadr004@hammer.cs.ucr.edu>
2016-11-18 10:28:25 -0800

    updated cpp h and make file errors

--- Makefile
+++ Makefile
@@ -8,9 +8,9 @@ OBJS = ./bin/Userin.o
 all: ./src/main.cpp $(OBJS)
 	$(COMPILE) $(FLAGS) -g ./src/main.cpp $(OBJS) -o ./bin/rshell
 
-./bin/Userin.o: ./src/Userin.h 
+./bin/Userin.o: ./src/Userin.h ./src/Userin.cpp 
 	mkdir -p ./bin
-	$(COMPILE) $(FLAGS) -c ./src/Userin -o
+	$(COMPILE) $(FLAGS) -c ./src/Userin.cpp -o ./src/Userin.o
 
 clean:
 	rm -rf ./bin/
--- src/Userin.h
+++ src/Userin.h
@@ -16,17 +16,17 @@ using namespace std;
 
 class userin {
 protected:
-	string test1 = "ls -a; echo hello && mkdir test || echo world; git status";
+	string test1;
 	//string test1= " ";
 	std::vector <string > comarg;
 	std::vector <string> con;
 	std::vector <string> callf;
 	std::string builder;
 	std::string connector;
-	int j = 0;
-	int k = 0;
+	int j;
+	int k;
 	char conectest;
-	int valoc = 0;
+	int valoc;
 
 
 public:
@@ -39,201 +39,5 @@ public:
 	void funct(vector<string> test1);
 };
 
-void userin::parse() {
-	for (unsigned int i = 0; i < test1.size(); i++)
-	{
-		conectest = test1[i];
-		valoc = i;
 
-		if ((conectest == ';'))
-		{
-			this->comarg.push_back(builder);
-			builder.clear();
-			connector += ';';
-			this->con.push_back(connector);
-			connector.clear();
-			k = 1;
-		}
-		else if ((conectest == '&'))
-		{
-			this->comarg.push_back(builder);
-			builder.clear();
-			i++;
-			k = 1;
-			connector += '&';
-			connector += '&';
-			this->con.push_back(connector);
-			connector.clear();
-		}
-		else if ((conectest == '|'))
-		{
-			this->comarg.push_back(builder);
-			builder.clear();
-			i++;
-			k = 1;
-			connector += '|';
-			connector += '|';
-			this->con.push_back(connector);
-			connector.clear();
-		}
-		else if ((conectest == ' '))
-		{
-			if (builder.size() > 1)
-			{
-				builder += ' ';
-			}
-		}
-		else
-		{
-			builder += conectest;
-		}
-	}
-	this->comarg.push_back(builder);
-	connector = ';';
-	this->con.push_back(connector);
-}
-
-void userin::funct(vector<string> test1)
-{
-
-	char *cstr = new char[test1[0].length() + 1];
-	strcpy(cstr, test1[0].c_str());
-
-	char * val1[] = { cstr };
-	char * name[] = {
-		"/bin/bash",
-		"-c",
-		cstr,
-		NULL
-	};
-	pid_t child_pid;
-	pid_t c;
-	int cstatus;
-	child_pid = fork();
-	if(child_pid ==-1)
-	{
-		fprintf(stderr, "Fork Failed. \n");
-		_exit(1);
-	}
-	if(child_pid == 0)
-	{
-	execvp(name[0], name);
-	fprintf(stderr, "child process could not do execvp.\n");
-	_exit(1);
-	}
-	else
-	{
-	waitpid(child_pid, &cstatus, 0);
-	printf("Parent: child %ld exited with status = %d\n", (long)c, cstatus);
-	}
-	
-	return;
-
-}
-void userin::print1()
-{
-	std::cout << std::endl << "the string entered is: " << std::endl << this->test1 << std::endl;
-
-	std::cout << std::endl << "the command/argument string vector is as follow: " << std::endl;
-	for (unsigned int i = 0; i < this->comarg.size(); i++)
-	{
-		std::cout << "[" << i << "]" << comarg.at(i) << std::endl;
-	}
-	std::cout << std::endl;
-
-	std::cout << "the connector string vector is as follow: " << std::endl;
-	for (unsigned int k = 0; k < this->con.size(); k++)
-	{
-		std::cout << "[" << k << "]" << con.at(k) << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-
-
-void userin::callfunct()
-{
-	builder.clear();
-	string loader;
-	string nextloader;
-	string nextnextloader;
-	char a;
-	char b;
-	bool booltest = false;
-
-	for (unsigned int i = 0; i < this->comarg.size(); i++)
-	{
-
-		//at this point loader has a string in it and nextloader has the next string in it <-- if there even is a next string
-
-		for (unsigned int j = 0; j < this->con.size(); j++)
-		{
-			loader = comarg.at(i);
-			if (i + 1 < this->comarg.size())
-			{
-				nextloader = comarg.at(i + 1);
-			}
-			if (i + 2 < this->comarg.size())
-			{
-				nextnextloader = comarg.at(i + 2);
-			}
-			a = this->con[j].at(0);
-			if (j + 1 < this->con.size())
-			{
-				b = this->con[j + 1].at(0);
-			}
-			// at this point a has first connector ans b has the second connector  <-- if there even is a next connector
-
-			if (a == ';')//call funct and pass loader as an argument
-			{
-
-				callf.push_back(loader);
-				//std::cout << loader << std::endl;
-				this->funct(callf);
-				callf.clear();
-				i++;
-			}
-			else if (a == '&')//call funct and pass loader as first arg and nextloader as second arg
-			{
-
-				//execute loader then
-				callf.push_back(loader);
-				//std::cout << loader << std::endl;
-				this->funct(callf);
-				if (booltest == false)
-					callf.clear();
-				j++;
-				i++;
-
-				//execute loader then
-				callf.push_back(nextloader);
-				//std::cout << nextloader << std::endl;
-				this->funct(callf);
-				callf.clear();
-				i++;
-
-
-				//execute nextloader fails look at b
-				if (booltest == false && b == '|')
-				{
-					j++;
-					// run nextnextloader
-					callf.push_back(nextnextloader);
-					//std::cout << nextnextloader << std::endl;
-					this->funct(callf);
-					callf.clear();
-					i++;
-				}
-				else
-				{
-
-				}
-			}
-			else if (a == '|') //call funct and pass first argument   !! IF !! it fails then pass second argument
-			{
-				//needs to be the same code as the b== '|' up there ^^^^
-			}
-		}
-	}
-}
 #endif
