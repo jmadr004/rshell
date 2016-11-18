e2d56c14fe497056c029bb27b88174082905e862
ralme002 <ralme002@ucr.edu>
2016-11-18 15:11:04 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,28 @@
 #include <vector>
 #include <string>
 #include "Userin.h"
+#include <string>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
+	string val1 = "(echo A && echo B) || (echo C && echo D)";
+	//getline(cin, val1);
+
+	vector<string> kal1;
+
+
+	
 	userin temp1;
-	temp1.fill();
-	temp1.parse();
-	temp1.print1();
-	temp1.callfunct();
+	//temp1.parse(val1);
+	//temp1.printall(val1);
+	//temp1.callfunct();
+
+	cout << endl << "--ASSIGNMENT 3 TESTING -- " << endl;
+	temp1.test1(val1);
+
+
 	return 0;
 }
