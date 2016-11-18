014bc1bc094675b1c6c713107c7403512a4b205f
Joshua Madrid <jmadr004@hammer.cs.ucr.edu>
2016-11-17 16:02:54 -0800

    please work

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Userin.h"
+
+using namespace std;
+
+int main()
+{
+	userin temp1;
+	//temp1.fill();
+	temp1.parse();
+	temp1.print1();
+	temp1.callfunct();
+	return 0;
+}
