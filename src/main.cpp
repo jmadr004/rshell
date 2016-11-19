0e7e9e47edb5cfde2650b4245f6561be489747c2
ralme002 <ralme002@ucr.edu>
2016-11-18 23:02:50 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,26 @@ using namespace std;
 
 int main()
 {
-	string val1 = "(echo A && echo B) || (echo C && echo D)";
-	//getline(cin, val1);
+//string val1 = "(echo A && echo B) || (echo C && echo D)";
+	getline(cin, val1);
 
 
 	//vector<string> val1;
 
 	
 	userin temp1;
-	temp1.parse(val1);
-	temp1.printall(val1);
-	temp1.callfunct();
+	//temp1.parse(val1);
+	//temp1.printall(val1);
+	//temp1.callfunct();
 
-	cout << endl << "--ASSIGNMENT 3 TESTING -- " << endl;
 	//temp1.test1(val1);
 	//temp1.parse(val1);
 	//temp1.printall(val1);
 	//temp1.callfunct();
 
-	cout << endl << "--ASSIGNMENT 3 TESTING -- " << endl;
-	temp1.test1(val1);
+	//temp1.test1(val1);
+
+	temp1.functest0(val1);
 
 	return 0;
 }
